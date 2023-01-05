f8e42528bd56d198bcdb220a8c21ac2b4fb0c1ba
Oswald Toma <oswald.toma@gmail.com>
2023-01-05 17:04:10 +0100

    Add ds18b20 lib

--- main/main.cpp
+++ main/main.cpp
@@ -1,12 +1,22 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
 #include "esp_log.h"
+#include "../lib/ds18b20/ds18b20.h"
 
 extern "C"
 {
+    void mainTask(void *pvParameters)
+    {
+        ds18b20_init(4);
+        while(1)
+        {
+            printf("Temp: %0.1f\n", ds18b20_get_temp());
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+    }
+
     void app_main(void)
     {
-
+        xTaskCreatePinnedToCore(&mainTask, "mainTask", 2048, NULL, 5, NULL, 0);        
     }
 }
